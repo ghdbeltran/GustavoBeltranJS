d250ff7d2b2ed01425b8335dbdfdb59a6e718b5d
Gustavo Beltran <gbeltran.onorato@gmail.com>
2022-06-17 16:00:04 -0300

    algoritmo

---
+++ index.js
@@ -0,0 +1,8 @@
+let ingresarNumero = parseInt(prompt("Aprendemos las tablas"));
+
+for (let i = 1; i <= 10; i++) {
+    
+    let resultado = ingresarNumero * i ;
+    alert(ingresarNumero +" X "+ i + "= "+ resultado);
+
+}
